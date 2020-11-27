459a9e021ffb8995ecc3ca18249f617699629c01
weilan <17796646068@163.com>
2020-11-27 16:26:22 +0800

    feat: 更新引入线上版本

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import "./plugins/element.js";
 
 /* import wl from "wl-vue-select";
 import "wl-vue-select/lib/wl-vue-select.css"
-Vue.use(wl);
- */
+Vue.use(wl); */
+
 Vue.config.productionTip = false;
 
 new Vue({
